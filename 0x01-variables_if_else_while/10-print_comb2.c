d003eb2d45cca61aea86cf876ab3856dd5fdd14d
Santiago <santitoya-2001@outlook.com>
2019-06-20 23:01:20 +0000

    Try 8-9-10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Print 0-99
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+putchar(i + 48);
+putchar(j + 48);
+if (i * j != 81)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+putchar(10);
+return (0);
+}
