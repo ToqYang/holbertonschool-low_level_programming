e0641521241855415e6cfc894c1b525a22641ddb
Santiago <santitoya-2001@outlook.com>
2019-06-20 21:39:56 +0000

    Print number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num_bdiez;
+num_bdiez = 0;
+while (num_bdiez < 10)
+{
+printf("%d", num_bdiez);
+num_bdiez++;
+}
+putchar('\n');
+return (0);
+}
