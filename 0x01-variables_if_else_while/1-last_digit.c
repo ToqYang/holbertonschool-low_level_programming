f5391005c8569244ecab08b10086abc28edce811
Santiago <santitoya-2001@outlook.com>
2019-06-20 20:23:42 +0000

    Try four last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,21 +9,23 @@
 int main(void)
 {
 int n;
-
+int q;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("Last digit of %d", n);
-if (n > 5)
+q = n % 10;
+printf("Last digit of %d is %d", n, q);
+
+if (q > 5)
 {
-printf(" and is %d greater than 5\n", n % 10);
+printf(" and is greater than 5\n");
 }
-else if (n < 6 && n > 0)
+else if (q < 6 && q != 0)
 {
-printf(" and is %d less 6 and not 0\n", n % 10);
+printf(" and is less than 6 and not 0\n");
 }
 else
 {
-printf(" is %d and is 0\n", n % 10);
+printf(" and is 0\n");
 }
 return (0);
 }
