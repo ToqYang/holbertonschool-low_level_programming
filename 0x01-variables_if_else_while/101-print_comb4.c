356692bf97053326a104f5cbbd4b48be3d312b8f
Santiago <santitoya-2001@outlook.com>
2019-06-21 02:44:16 +0000

    Try advance :)

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+* main - entry point of function
+*
+* Return : 0
+**/
+int main(void)
+{
+int a, b, c;
+a = '0';
+while (a <= '7')
+{
+b = a + 1;
+while (b <= '8')
+{
+c = b + 1;
+while (c <= '9')
+{
+putchar(a);
+putchar(b);
+putchar(c);
+if (a != '7')
+{
+putchar(',');
+putchar(' ');
+}
+c++;
+}
+b++;
+}
+a++;
+}
+putchar('\n');
+return (0);
+}
