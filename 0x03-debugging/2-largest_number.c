2157808f58e77400aec33fc0a0ac3143d66f18aa
Santiago Yanguas <santitoya-2001@outlook.com>
2019-06-26 01:29:25 +0000

    All the files 0-3

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * largest_number - Return largest
+ * @a: First number
+ * @b: Second number
+ * @c: Third number
+ * Return: number largest
+ */
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a >= b && a >= c)
+		largest = a;
+	else if (b >= a && b >= c)
+		largest = b;
+	else
+		largest = c;
+
+	return (largest);
+}
