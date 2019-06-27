aa28ef5c75f85208106b46ad573f22d6916caaa0
Santiago Yanguas <santitoya-2001@outlook.com>
2019-06-27 20:29:02 +0000

    Add fizz-buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Print the multiples of 3 and 5
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+
+		if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("Fizzbuzz ");
+		}
+		else if (a % 3 == 0 && a % 5 != 0)
+		{
+			printf("Fizz ");
+		}
+		else if (a % 5 == 0 && a % 3 != 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+	}
+	printf("\n");
+	return (0);
+}
