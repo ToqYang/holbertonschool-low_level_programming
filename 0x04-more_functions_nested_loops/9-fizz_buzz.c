7682aa45e3a1a5c1d6ed08b5c346f7242c767773
Santiago <45366195+ToqYang@users.noreply.github.com>
2019-06-27 22:39:16 -0500

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,18 +7,18 @@ int main(void)
 {
 	int a;
 
-	for (a = 1; a <= 100; a++)
+	for (a = 1; a < 101; a++)
 	{
 
-		if (a % 3 == 0 && a % 5 == 0)
+		if (a % 15 == 0)
 		{
 			printf("FizzBuzz");
 		}
-		else if (a % 3 == 0 && a % 5 != 0)
+		else if (a % 3 == 0)
 		{
 			printf("Fizz");
 		}
-		else if (a % 5 == 0 && a % 3 != 0)
+		else if (a % 5 == 0)
 		{
 			printf("Buzz");
 		}
