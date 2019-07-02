6d8131004d663d3ceb0627f5811117dbe5c327bb
Santiago Yanguas <santitoya-2001@outlook.com>
2019-07-02 20:09:40 +0000

    Add pairs numbers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * rev_string - Print the message of function
+ * @s: Pointer of a char
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	char a[500];
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		a[i] = *s;
+	}
+
+	while (i--)
+	{
+	}
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * puts2 - Print the pairs
+ * @s: Store the value char
+ */
+
+void puts2(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if(s[i] % 2 == 0)
+		{
+			_putchar(*(s+i));
+		}
+	}
+	_putchar('\n');
+}
