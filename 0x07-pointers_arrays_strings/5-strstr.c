292d9b8b228a0687659f6e112167087119e25830
Santiago Yanguas <santitoya-2001@outlook.com>
2019-07-08 23:07:47 +0000

    Fix betty

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,5 +9,14 @@
 
 char *_strstr(char *haystack, char *needle)
 {
+		int i;
 
+		for (i = 0; haystack[i] != '\0'; i++)
+		{
+			if (haystack[i] == needle[i])
+			{
+				return (needle);
+			}
+		}
+		return (0);
 }
---
+++ 0x07-pointers_arrays_strings/6-print_chessboard.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - Print the chessboard
+ * @a: Store the array
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; a[8] != '\0')
+}
