a67b1d751f17dbec39f9a9eb6b56fe33b9006906
Santiago <45366195+ToqYang@users.noreply.github.com>
2019-07-10 23:28:48 -0500

    [Update] Check prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,40 +1,40 @@
 #include "holberton.h"
 
 /**
- * is_prime_number - Prime number
- * @n: prime number
- * Return: Nothing
+ * _check - Check if is prime
+ * @a: Number one
+ * @b: Iterations
+ * Return: 1 prime 0 Not prime
+ */
+
+int _Check_Prime(int a, int b)
+{
+	if ((a % b == 0) && (a == b))
+	{
+		return (1);
+	}
+	else if ((a % b == 0) && (b < a))
+	{
+		return (0);
+	}
+	return (_Check_Prime(a, b + 1));
+}
+
+/**
+ * is_prime_number - Print the prime number
+ * @n: Store for test if is prime
+ * Return: Integer
  */
 
 int is_prime_number(int n)
 {
-	if (n <= 1)
+	if (n == 1)
 	{
 		return (0);
 	}
-	else if (n == 2)
-	{
-		return (1);
-	}
-	return (prime(n, 2));
-}
-
-/**
- * prime - Check if is prime
- * @n: integer
- * @i: Counter
- * Return: Integer
- */
-
-int prime(int n, int i)
-{
-	if (i % n == 0)
+	else if (n <= 0)
 	{
 		return (0);
 	}
-	else if (n + 1 == i  && i % n != 0)
-	{
-		return (1);
-	}
-	return (prime(n + 1, i));
+	return (_Check_Prime(n, 2));
 }
