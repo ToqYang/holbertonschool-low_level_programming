bd0fa4c14f91ef42d0e9a36fc40aecbf2a99ba65
Santiago <45366195+ToqYang@users.noreply.github.com>
2019-07-10 23:23:52 -0500

    [Update] Square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,36 +1,47 @@
 #include "holberton.h"
 
 /**
- * _sqrt_recursion - Print square root
- * @n: Number
- * Return: the natural square root of a number
+ * _func - Print square root
+ * @a: Store the first number
+ * @b: ..  .. .. second number
+ * Return: int
+ */
+
+int _func(int a, int b)
+{
+	if ((b * b) == a)
+	{
+		return (b);
+	}
+	else if ((b * b) < a)
+	{
+		return (_func(a, b + 1));
+	}
+	else
+	{
+		return (-1);
+	}
+}
+
+/**
+ * _sqrt_recursion - Verify square root
+ * @n: Store number
+ * Return: Integer
  */
 
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
+	{
 		return (-1);
+	}
 	else if (n == 0)
+	{
 		return (0);
+	}
 	else if (n == 1)
+	{
 		return (1);
-	else
-		return (cal_sq_ro(n, 1));
-}
-
-/**
- * cal_sq_ro - Iterative function
- * @n: Integer for prove the square root
- * @i: Exponent
- * Return: Square root;
- */
-
-int cal_sq_ro(int n, int i)
-{
-	if (n == (i * i))
-		return (i);
-	else if (n > (i * i))
-		return (cal_sq_ro(n, i + 1));
-	else
-		return (-1);
+	}
+		return (_func(n, 2));
 }
