c96796b9fed820b2b6aca4af48d7710385522995
Santiago <santitoya-2001@outlook.com>
2019-07-14 20:58:04 +0000

    [Add] c=a*b

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print the multiplication of arguments
+ * @argc: Count the number of the arguments
+ * @argv: Ptr to string of count
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i, res;
+
+	i = 0;
+	res = 1;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		for(i = 1; i < argc; i++)
+		{
+			res *= atoi(argv[i]);
+		}
+		printf("%d", res);
+	}
+
+        return (0);
+}
