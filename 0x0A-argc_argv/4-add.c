c7c55ffd809283c66fff21dec034808ea85a52ba
Santiago <santitoya-2001@outlook.com>
2019-07-14 23:06:57 +0000

    [Add] Positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print the positive numbers
+ * @argc: Count the number of the arguments
+ * @argv: Ptr to string of count
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i, res;
+
+	i = 0;
+	res = 0;
+
+	if (argc == 1)
+	{
+	        printf("%d\n", res);
+	}
+
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			    if (!(*argv[i] >= 48 && *argv[i] <= 57))
+			    {
+				    printf("Error\n");
+				    return (1);
+			    }
+
+			    res += atoi(argv[i]);
+		}
+		printf("%d\n", res);
+	}
+
+	return (0);
+}
