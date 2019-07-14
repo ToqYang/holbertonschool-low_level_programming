021193f6ed072940a54ca122fa219260588b2d56
Santiago <santitoya-2001@outlook.com>
2019-07-14 23:19:32 +0000

    [Fix] Betty-Style

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,25 +15,23 @@ int main(int argc, char **argv)
 	i = 0;
 	res = 0;
 
-	if (argc == 1)
+	if (argc <= 1)
 	{
-	        printf("%d\n", res);
+		printf("%d\n", res);
 	}
-
 	else
 	{
 		for (i = 1; i < argc; i++)
 		{
-			    if (!(*argv[i] >= 48 && *argv[i] <= 57))
-			    {
-				    printf("Error\n");
-				    return (1);
-			    }
+			if (!(*argv[i] >= 48 && *argv[i] <= 57))
+			{
+				printf("Error\n");
+				return (1);
+			}
 
-			    res += atoi(argv[i]);
+			res += atoi(argv[i]);
 		}
 		printf("%d\n", res);
 	}
-
 	return (0);
 }
