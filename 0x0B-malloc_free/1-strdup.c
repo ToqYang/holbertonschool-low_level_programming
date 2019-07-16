afb85ef2d3ca72f9f63ff398ef4010b00bc4a8cc
Santiago Yanguas <santitoya-2001@outlook.com>
2019-07-16 21:45:26 +0000

    [Solution] Duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Print the duplicate
+ * @str: Store char
+ * Return: Pointer
+ */
+
+char *_strdup(char *str)
+{
+	int size, i;
+	char *copy;
+
+	size = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (size = 1; str[size] != '\0'; size++)
+	{
+		;
+	}
+	size++;
+	copy = malloc(sizeof(char) * size);
+
+	for (i = 0; i < size; i++)
+	{
+		copy[i] = str[i];
+	}
+
+	return (copy);
+}
