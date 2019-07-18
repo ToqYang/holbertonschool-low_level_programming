0fd60835db99d60e4b4013f26bf750f0724ae43d
Santiago Yanguas <santitoya-2001@outlook.com>
2019-07-18 18:56:20 +0000

    [Solution] Concat strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * string_nconcat - Print the concatenation
+ * @s1: String one
+ * @s2: String the second about n bytes
+ * @n: Store the size of second string
+ * Return: Pointer to pointer
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+        unsigned int i, j;
+	char *dest;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+
+	dest = malloc((n + i + 1) * sizeof(char));
+
+	if (dest == NULL)
+		return (NULL);
+
+	for (j = 0; j < i; j++)
+		dest[j] = s1[j];
+
+	for (j = 0; j < n; j++)
+		dest[j + i] = s2[j];
+
+	dest[i + j + 1] = 0;
+
+	return (dest);
+}
