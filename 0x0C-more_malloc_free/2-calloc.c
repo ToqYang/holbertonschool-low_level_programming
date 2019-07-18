239c3e08e6abd580b1f57cce450af27c392c5be8
Santiago Yanguas <santitoya-2001@outlook.com>
2019-07-18 20:06:59 +0000

    [Solution] Function calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _calloc - Print allocate memory
+ * @nmemb: Store size
+ * @size: Store the data size
+ * Return: Ptr to Ptr
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *memset;
+
+	int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	memset = malloc(nmemb * size);
+
+	if (memset == NULL)
+		return (NULL);
+
+	for (i = 0; memset[i] != '\0'; i++)
+	{
+		memset[i] = 0;
+	}
+
+	return (memset);
+}
