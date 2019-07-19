6a61758b2066fb273b360ff44ce65e2c60664ac4
Santiago <santitoya-2001@outlook.com>
2019-07-19 04:20:52 +0000

    [Update] It doesn't free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,26 +11,23 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *cpy_allocate;
-
-	unsigned int i;
 
 	if (ptr == NULL)
-		cpy_allocate = malloc(new_size);
+		ptr = malloc(new_size);
+
 	if (new_size > old_size)
-		cpy_allocate = malloc(new_size);
+	{
+		ptr = malloc(new_size);
 
-		if (cpy_allocate == NULL)
+		if (ptr == NULL)
 			return (NULL);
-
-		for (i = 0; i < old_size; i++)
-			cpy_allocate[i] = ptr[i];
+	}
 
 	if (new_size == old_size)
 		return (ptr);
-	if (new_size == 0 && ptr != 0)
-		free(ptr);
 
-	free(ptr);
-	return (cpy_allocate);
+	if (new_size == 0 && ptr != 0)
+		return (NULL);
+
+	return (ptr);
 }
