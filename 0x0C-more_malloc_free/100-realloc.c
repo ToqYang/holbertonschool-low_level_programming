8612e496dd2cc25786958f6368014b769b491eec
Santiago Yanguas <santitoya-2001@outlook.com>
2019-07-19 19:22:25 +0000

    [Solution] Free memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,23 +11,23 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	char *a;
 
 	if (ptr == NULL)
-		ptr = malloc(new_size);
-
-	if (new_size > old_size)
-	{
-		ptr = malloc(new_size);
-
-		if (ptr == NULL)
-			return (NULL);
-	}
+		return (malloc(new_size));
 
 	if (new_size == old_size)
 		return (ptr);
 
 	if (new_size == 0 && ptr != 0)
+	{
+		free(ptr);
 		return (NULL);
 
-	return (ptr);
+	}
+
+	free(ptr);
+	a = malloc(new_size);
+
+	return (a);
 }
