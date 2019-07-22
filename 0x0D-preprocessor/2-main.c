4c7f4d8d2bdb7c2da59b9f653f722fb6e26de82e
Santiago Yanguas <santitoya-2001@outlook.com>
2019-07-22 20:27:22 +0000

    [Add] MacroFile

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Print the name file
+ * @argc: Count arguments
+ * @argv: Value arguments
+ * Return: Nothing
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
