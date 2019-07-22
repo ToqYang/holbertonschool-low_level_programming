7a558ec97179531cb78b6637335e30cbea99fc2f
Santiago Yanguas <santitoya-2001@outlook.com>
2019-07-22 22:27:42 +0000

    [Add-File] Django

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MY_DOGS
+#define MY_DOGS
+
+/**
+ * struct dog - Object dog
+ * @name: Store char name
+ * @age: Store float age
+ * @owner: Store the ownwe dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
