96aaf89d99b3ff95d55206f8af83ccfa28fb38fe
Santiago Yanguas <santitoya-2001@outlook.com>
2019-07-24 19:59:42 +0000

    [Add-File-Solution] Search index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - Print the int index
+ * @array: Print the index of the array
+ * @size: Store the size of array
+ * @cmp: Function pointer compare
+ * Return: Integer index
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, index;
+
+	if (array != NULL && cmp != NULL)
+	{
+		if (size <= 0)
+			return (-1);
+
+		for (i = 0; i < size; i++)
+		{
+			if ((*cmp)(array[i]))
+			{
+				index = i;
+				break;
+			}
+		}
+
+		if (i == size)
+			return (-1);
+	}
+	return (index);
+}
