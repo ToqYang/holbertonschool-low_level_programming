0595ac7c8f7e4e3ddce7a627b03ec322cf34e8ca
Santiago Yanguas <santitoya-2001@outlook.com>
2019-07-26 18:43:10 +0000

    [Update] Print the numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Print numbers followed new line
+ * @separator: String print between numbers
+ * @n: Store the size argumenst
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+
+	unsigned int i;
+
+	va_start(list, n);
+
+	if (n > 0 && separator)
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (i < (n - 1))
+			{
+				printf("%d%s", va_arg(list, int), separator);
+			}
+			else
+			{
+				printf("%d\n", va_arg(list, int));
+			}
+		}
+		va_end(list);
+	}
+}
