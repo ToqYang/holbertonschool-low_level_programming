96f2fc21a710e4592b9baff7c446665db74ed230
Santiago Yanguas <santitoya-2001@outlook.com>
2019-08-01 23:28:53 +0000

    [Add] Solution turtle and hare

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_before - Print before main function
+ * void: Not take nothing
+ * Return: voyd type
+ */
+void print_before(void) __attribute__ ((constructor));
+
+void print_before(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
