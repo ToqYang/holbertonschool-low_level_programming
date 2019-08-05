cb85567be8bb5c5dbe68651e0f3d0cc77791482b
Santiago Yanguas <santitoya-2001@outlook.com>
2019-08-05 21:51:40 +0000

    [Fix] Betty-Style

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * add_nodeint - Print the initial node
+ * @head: Store the ptr to struct
+ * @n: Passed argument
+ * Return: listint_t
+ */
+
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *tmp;
