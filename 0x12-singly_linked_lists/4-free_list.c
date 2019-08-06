14accd73845a6244d131f020bc33c4c39a8cd066
Santiago Yanguas <santitoya-2001@outlook.com>
2019-08-06 19:23:26 +0000

    [Fix] Betty-Style

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,14 +1,20 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * free_list - Free memory of linked list
+ * @head: Store direction of struct
+ * Return: Nothing
+ */
+
 void free_list(list_t *head)
 {
-        list_t *current;
+	list_t *current;
 
-        while (head != NULL)
-        {
-                current = head->next;
+	while (head != NULL)
+	{
+		current = head->next;
 		free(head);
-                head = current;
-        }
+		head = current;
+	}
 }
