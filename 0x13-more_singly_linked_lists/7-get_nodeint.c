b75ec8203a951c2c8e328e1b3975825e6438312d
Santiago Yanguas <santitoya-2001@outlook.com>
2019-08-06 21:52:59 +0000

    [Fix] Condition NULL

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,18 +12,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i;
 	listint_t *get_Node;
 
-	if (head == NULL)
-	{
-		return (NULL);
-	}
-
 	get_Node = head;
 
 	for (i = 0; i < index; i++)
 	{
 		if(head == NULL)
 		{
-			return (0);
+			return (NULL);
 		}
 		get_Node = get_Node->next;
 	}
