bcae7f50d605d2ce16133891238bb7d53fdecd1e
Santiago Yanguas <santitoya-2001@outlook.com>
2019-08-06 00:57:48 +0000

    [Add-Files] Delete head node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Print the head node
+ * @head: Double ptr
+ * Return: int
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int tmp_Num;
+
+	if (*head == NULL)
+	{
+		return (0);
+	}
+
+	tmp = (*head)->next;
+	tmp_Num = (*head)->n;
+
+	free(tmp);
+
+	*head = tmp;
+
+	return (tmp_Num);
+}
