170294430027d842b5fcbc5e502defeb28bf3f8c
Santiago Yanguas <santitoya-2001@outlook.com>
2019-08-07 02:27:40 +0000

    [Add-Files] Delete node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	if (*head != NULL)
+	{
+		listint_t *temp, *node;
+		int count;
+
+		for (count = 0; count < index; count++)
+		{
+			temp->
+		}
+
+		return (1);
+	}
+	return (-1)
+}
