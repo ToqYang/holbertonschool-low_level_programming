f4bd730c5c90c1736a858e9894e3d8671405e987
Santiago Yanguas <santitoya-2001@outlook.com>
2019-08-08 23:46:38 +0000

    [Add-Files] Solution binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,3 +1,5 @@
+#include "holberton.h"
+
 /**
  * binary_to_uint - Print binary to int
  * @b: Pointer to char to convert int
@@ -6,5 +8,28 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	int i, result;
 
+	i = 0;
+	result = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == '1')
+		{
+			result = ((result * 2) + 1);
+		}
+		else if (b[i] == '0')
+		{
+			result = (result * 2);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (result);
 }
