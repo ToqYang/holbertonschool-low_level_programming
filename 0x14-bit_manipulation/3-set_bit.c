3eae6e3972ff545d2fab895c26b5edc17b984ca9
Santiago Yanguas <santitoya-2001@outlook.com>
2019-08-09 02:30:02 +0000

    [Update] Set value in 1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,5 +1,7 @@
+#include "holberton.h"
+
 /**
- * set_bits: Set bit to 1
+ * set_bit - Set the number bit to 1
  * @n: Set the bit about the index
  * @index: Return the index
  * Return: Integer index
@@ -7,5 +9,13 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned int mask_num = 1;
 
+	if (index > 63)
+		return (-1);
+
+	mask_num <<= index;
+	*n |= mask_num;
+
+	return (1);
 }
