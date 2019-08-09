59ed0cad3302b31f48cc5354b1a27a8f01054375
Santiago Yanguas <santitoya-2001@outlook.com>
2019-08-09 02:49:26 +0000

    [Update] Pass 1 to 0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,5 +7,13 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned int mask_num = 1;
 
+	if (index > 63)
+		return (-1);
+
+	mask_num = ~(mask_num << index);
+	*n &= mask_num;
+
+	return (1);
 }
