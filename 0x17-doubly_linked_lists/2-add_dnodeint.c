824d2cc3d896fb54d3eda7a0366e92d9a116961f
Santiago Yanguas <santitoya-2001@outlook.com>
2019-09-13 20:42:11 +0000

    [Fix] Add previous node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,7 +18,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new->next = *head;
 	new->n = n;
+	new->prev = NULL;
 	*head = new;
+
+	if (new->next != NULL)
+		new->next->prev = new;
 
 	return (new);
 }
