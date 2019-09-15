5baf11f4fa2b0bfc5e9b0983fdf7b073f3ee7d93
Santiago <45366195+ToqYang@users.noreply.github.com>
2019-09-15 13:24:14 -0500

    [Add-File] Node end

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - Add node to the end
+ * @head: Store the pointer to pointer to structure
+ * @n: Store the intege
+ * Return: node of type dlistint_t
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_Node;
+	dlistint_t *new_Last;
+
+	new_Node = malloc(sizeof(dlistint_t));
+
+	if (new_Node == NULL)
+		return (NULL);
+
+	new_Node->n = n;
+	new_Node->prev = NULL;
+	new_Last = *head;
+
+	if (*head == NULL)
+	{
+		*head = new_Node;
+		return (new_Node);
+	}
+	while (new_Last->next != NULL)
+	{
+		new_Last = new_Last->next;
+	}
+	new_Last->next = new_Node;
+	
+	if (new_Node->next != NULL)
+		new_Node->next->prev = new_Node;
+	
+	return (new_Node);
+}
