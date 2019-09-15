194c01758b26766341b5f7e041eca9d34459447a
Santiago <45366195+ToqYang@users.noreply.github.com>
2019-09-15 13:34:16 -0500

    [Add-File] Free node

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Free node
+ * @head: Store the ptr at the node to free
+ * Return: Nothing
+ */
+
+void free_dlistint(dlistint_t *head);
+{
+	dlistint_t *del_Node;
+
+	while (head != NULL)
+	{
+		del = head->next;
+		free(head);
+		head = del_Node;
+	}
+}
