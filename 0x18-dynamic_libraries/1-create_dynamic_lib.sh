6fd7313fad11dad360f95c3be9d2739a8d8343dd
Santiago Yanguas <santitoya-2001@outlook.com>
2019-09-16 17:20:13 +0000

    [Fix] Add WL soname

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gcc gcc -Wall -Werror -Wextra -pedantic -fPIC -c *.c
-gcc -shared *.o -o liball.so
+gcc -shared -Wl,-soname,liball.so -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
