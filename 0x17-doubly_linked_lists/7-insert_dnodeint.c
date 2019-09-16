063bfe75fbc621e8a5f7026247035e046cd06704
Santiago <45366195+ToqYang@users.noreply.github.com>
2019-09-15 22:51:06 -0500

    Create 7-insert_dnodeint.c

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - Insert node about index
+ * @h:Address node
+ * @idx: Number of the index
+ * @n: Number node
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+
+}
