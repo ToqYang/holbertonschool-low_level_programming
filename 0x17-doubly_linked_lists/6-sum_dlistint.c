15378164d81c950562636b3a69ba5e18c5d8e8c2
Santiago Yanguas <santitoya-2001@outlook.com>
2019-09-16 21:57:26 +0000

    [Fix] Initialized in Null

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -3,22 +3,22 @@
 /**
  * sum_dlistint - Sum all nodes
  * @head: Address of the node
- * Return: Result of the sum 
+ * Return: Result of the sum
  */
- 
+
 int sum_dlistint(dlistint_t *head)
 {
 	int result;
 	dlistint_t *ref_Node;
-	
+
 	result = 0;
-	ref_Node
+	ref_Node = NULL;
 
 	if (head == NULL)
 		return (0);
-		
+
 	ref_Node = head;
-	
+
 	while (ref_Node != NULL)
 	{
 		result += ref_Node->n;
