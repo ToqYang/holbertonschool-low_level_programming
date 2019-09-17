ee4452d0b164365240915576c4f6c11d8bec4902
Santiago Yanguas <santitoya-2001@outlook.com>
2019-09-17 00:17:17 +0000

    [Feature] Advance in code

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,5 +10,38 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint *new_Node;
+	dlistint *new_Last;
+	unsigned int count;
 
+	new_Node = NULL;
+	new_Last = NULL;
+	count = 0;
+
+	new_Node = malloc(sizeof(dlistint));
+
+	if (new_Node == NULL)
+		return (NULL);
+
+	new_Last = h;
+	new_Node = new_Pre;
+	new_Node->next = NULL;
+	new_Node->prev = NULL;
+
+	if (idx == 0)
+		return (new_Node);
+
+	while (count <= index)
+	{
+		++count;
+
+		h = h->next;
+	}
+
+	new_Last = new_Node->prev;
+	(new_Node->prev)->next = new_Node;
+
+
+
+	return (new_Node);
 }
