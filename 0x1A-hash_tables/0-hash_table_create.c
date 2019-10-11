921885a99299284a029d5d6c33a05aafbe6f13e5
Santiago Yanguas <santitoya-2001@outlook.com>
2019-10-11 16:22:12 +0000

    [Feature] Make a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,12 +1,24 @@
 #include "hash_tables.h"
 
-/*
- * hash_table_create: Make a hash table
+/**
+ * hash_table_create - Make a hash table
  * @size: of the hash table
  * Return: Space of the hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t
+	hash_table_t *table = NULL;
+
+	table = calloc(sizeof(hash_table_t), 1);
+
+	if (table == NULL)
+		return (NULL);
+
+	table->array = calloc(sizeof(hash_node_t), size);
+
+	if (table->array == NULL)
+		return (NULL);
+
+	return (table);
 }
