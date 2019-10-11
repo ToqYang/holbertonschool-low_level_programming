ed909d3c3f0d5392ffdd02f6d4da1c3c21ee3b30
Santiago Yanguas <santitoya-2001@outlook.com>
2019-10-11 15:54:21 +0000

    [Add] Folder

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/*
+ * hash_table_create: Make a hash table
+ * @size: of the hash table
+ * Return: Space of the hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t
+}
