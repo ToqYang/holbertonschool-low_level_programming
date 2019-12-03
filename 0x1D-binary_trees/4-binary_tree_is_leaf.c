11c190ad975973c9c8dfafca6130015f4cdaf615
Santiago Yanguas <santitoya-2001@outlook.com>
2019-12-03 22:06:32 +0000

    [Feature] is leaf?

---
+++ 0x1D-binary_trees/4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - Check if a node is leaf
+ * @node: Store the node at the pointer to verify
+ * Return: 1 if the node is leaf, 0 not is leaf
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	/** Veify if exit some child */
+	if (node->left != NULL || node->right != NULL)
+		return (0);
+	else
+		/** If the node is leaf */
+		return (1);
+}
