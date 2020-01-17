86597c2c204dd08ff7cbe4cee9f6b8321e02dc82
Santiago Yanguas <santitoya-2001@outlook.com>
2020-01-17 01:15:34 +0000

    [Feature] Binary search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+
+void print_array(int *array, unsigned int start, unsigned int end);
+/**
+ * binary_search - It search to the number to find
+ * @array: (int) Store the numbers
+ * @size: (size_t) Store the number of the size of the array
+ * @value: Store the value to compare
+ * Return: (int) with the index of the position to find
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	unsigned int med = size, left = 0, right = size - 1;
+
+	if (array == NULL && size < 1)
+		return (-1);
+
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		print_array(array, left, right);
+
+		/* Take the size of each stack of arrays */
+		med = ((left + right) / 2);
+
+		/* The medium is equal to value */
+		if (array[med] == value)
+			return (med);
+		/** Not look left way if is smaller than value*/
+		if (array[med] < value)
+			left = med + 1;
+		/* If the med is greater than value */
+		else
+			right = med - 1;
+	}
+
+	return (-1);
+}
+
+/**
+ * print_array - Print a array
+ * @array: Store several numbers
+ * @start: (int) Introduction to array
+ * @end: (int) Ending of the array
+ */
+void print_array(int *array, unsigned int start, unsigned int end)
+{
+	while (start <= end)
+	{
+		printf("%d", array[start]);
+		if (start == end)
+			printf(" ");
+		else
+			printf(", ");
+		++start;
+	}
+	printf("\n");
+}
